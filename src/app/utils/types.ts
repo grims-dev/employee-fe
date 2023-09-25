db095f6df1c7758299538dcf44939916139f3eac
grims-dev <ciarangrimshawdev@gmail.com>
2023-09-25 20:45:02 +0100

    feat: add Employee type definition

---
+++ src/app/utils/types.ts
@@ -0,0 +1,12 @@
+export type Employee = {
+    city: string,
+    department: string,
+    departmentId: string,
+    email: string,
+    fullName: string,
+    gender: string,
+    hireDate: string, // UTC format
+    id: string,
+    isPermanent: Boolean,
+    mobile: string, // UK format
+}

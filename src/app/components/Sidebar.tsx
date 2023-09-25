85a13bed646a8f48861ec83cdae1a86f67a8180c
grims-dev <ciarangrimshawdev@gmail.com>
2023-09-25 19:19:00 +0100

    style: tweak sidebar block

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,5 +1,3 @@
 export default function Sidebar() {
-    return <aside className="w-1/6 h-screen bg-theme-900 text-white">
-
-    </aside>;
+    return <aside className="w-1/5 h-screen bg-theme-900 text-white"></aside>;
 }

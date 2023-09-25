dbf07e21655de59810635df45164c49e6957d305
grims-dev <ciarangrimshawdev@gmail.com>
2023-09-25 20:46:32 +0100

    feat: import EmployeeTable in Main view

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,9 +1,12 @@
+import EmployeeTable from './EmployeeTable';
 import TopNav from './TopNav';
 
 export default function Main() {
     return (
         <main className="grow text-gray-900">
             <TopNav />
+
+            <EmployeeTable />
         </main>
     );
 }

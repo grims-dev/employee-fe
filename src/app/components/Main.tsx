1460cdd66a19a83aff987a2e5b3ca9bdcdf954d6
grims-dev <ciarangrimshawdev@gmail.com>
2023-09-26 20:52:20 +0100

    feat: add PageContent component

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,10 +1,12 @@
 import EmployeeTable from './EmployeeTable';
+import PageContent from './PageContent';
 import TopNav from './TopNav';
 
 export default function Main() {
     return (
         <main className="grow bg-theme-100 text-gray-900">
             <TopNav />
+            <PageContent />
             <EmployeeTable />
         </main>
     );
---
+++ src/app/components/PageContent.tsx
@@ -0,0 +1,12 @@
+import ContentContainer from './ContentContainer';
+
+export default function PageContent() {
+    return (
+        <div className="bg-white">
+            <ContentContainer>
+                {/* TODO: Add page icon */}
+                <h1>New Employee</h1>
+            </ContentContainer>
+        </div>
+    );
+}

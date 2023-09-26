70cab765310c14b8a137ffae6297e67cfa3adb47
grims-dev <ciarangrimshawdev@gmail.com>
2023-09-26 20:33:05 +0100

    style: make sidebar full height

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,3 +1,3 @@
 export default function Sidebar() {
-    return <aside className="w-1/5 h-screen bg-theme-900 text-white"></aside>;
+    return <aside className="w-1/5 h-full bg-theme-900 text-white"></aside>;
 }

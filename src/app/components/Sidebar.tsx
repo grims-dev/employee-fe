9f0c43785c34ebc447f20c40a4684a36c5cdf315
grims-dev <ciarangrimshawdev@gmail.com>
2023-09-26 20:43:12 +0100

    style: make sidebar stick to top of page

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,3 +1,3 @@
 export default function Sidebar() {
-    return <aside className="w-1/5 h-full bg-theme-900 text-white"></aside>;
+    return <aside className="w-1/5 h-screen sticky top-0 bg-theme-900 text-white"></aside>;
 }
